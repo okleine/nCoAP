6bfb2917cba8e316d810b284a4859cdbb856d1a6
Stefan Hueske <stefanhueske@googlemail.com>
2012-03-18 20:39:08 +0100

    Test for CoapMessage.getPayloadAsByteArray() added

--- src/test/java/de/uniluebeck/itm/spitfire/nCoap/message/CoapMessageTest.java
+++ src/test/java/de/uniluebeck/itm/spitfire/nCoap/message/CoapMessageTest.java
@@ -1,8 +1,13 @@
 package de.uniluebeck.itm.spitfire.nCoap.message;
 
+import com.google.common.base.Charsets;
 import de.uniluebeck.itm.spitfire.nCoap.message.header.Code;
+import java.io.UnsupportedEncodingException;
+import org.apache.commons.lang.CharSet;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 /**
  * Test of CoapMessage class.
  * @author Stefan Hueske
@@ -18,5 +23,15 @@ public class CoapMessageTest {
     public void testSetPayloadForDELETE() throws Exception {
         new CoapMessage(Code.DELETE) {}.setPayload("testpayload".getBytes("UTF8"));
     }
-   
+    
+    @Test
+    public void testgetPayloadAsByteArray() throws MessageDoesNotAllowPayloadException {
+        CoapMessage message = new CoapMessage(Code.CONTENT_205) {};
+        byte[] payload = "testpayload".getBytes(Charsets.UTF_8);
+        message.setPayload(ChannelBuffers.wrappedBuffer(payload));
+        assertArrayEquals(payload, message.getPayloadAsByteArray());
+        
+        message = new CoapMessage(Code.CONTENT_205) {};
+        assertNull(message.getPayloadAsByteArray());
+    }
 }
