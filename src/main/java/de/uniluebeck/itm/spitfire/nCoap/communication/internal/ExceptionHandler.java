7b65a3e2aaf65081af27ffbdd850496ee7753be1
okleine <kleine@itm.uni-luebeck.de>
2012-11-27 15:54:21 +0100

    Handling of malformed or unrecoginzed critical options in CON messages

---
+++ src/main/java/de/uniluebeck/itm/spitfire/nCoap/communication/internal/ExceptionHandler.java
@@ -0,0 +1,11 @@
+package de.uniluebeck.itm.spitfire.nCoap.communication.internal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: olli
+ * Date: 27.11.12
+ * Time: 13:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExceptionHandler {
+}
